51e9bed9407adb376ed2a5eaaa581bdb5a981f75
RobertGosa <31468427+RobertGosa@users.noreply.github.com>
2020-09-22 16:05:35 +0200

    Add files via upload

---
+++ Searching labb 3/src/Uppg1Filter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Uppg1Filter {
+    public static void uppg1Filter() {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()){
+            String string = scan.nextLine() + '\n';
+            for (int counter = 0;string.length() > counter; counter++) {
+                if (string.charAt(counter) == 32 || string.charAt(counter) > 64 && string.charAt(counter) < 91 || string.charAt(counter) > 96 && string.charAt(counter) < 123 ){
+                    System.out.print(string.charAt(counter));
+                } else if (string.charAt(counter) == '\n'){
+                    System.out.println();
+                } else {
+                    System.out.print(' ');
+                }
+            }
+        }
+    }
+}
